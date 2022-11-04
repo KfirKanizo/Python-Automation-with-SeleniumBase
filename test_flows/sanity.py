96a4a10ee5cc8bd5326e455d70bd6e23e55bf618
KfirKanizo <kfir.kanizo@outlook.com>
2022-11-05 00:55:05 +0200

    add setUp and tearDown

--- test_flows/sanity.py
+++ test_flows/sanity.py
@@ -6,9 +6,16 @@ from infrastructure.page_objects import general, txt_inputs, dropdowns, clicks, 
 
 class sanity(BaseCase):
 
-    def test_open_page(self):
-        # Open a web page in the active browser window
+    def setUp(self):
+        super().setUp()
+        print("************** STARTING SESSION **************")
         self.open(config.base_url)
+
+    def tearDown(self):
+        print("************** CLOSING SESSION **************")
+        super().tearDown()
+
+    def test_open_page(self):
         # Assert the title of the current web page
         self.assert_title("Web Testing Page")
         # Assert that an element is visible on the page
@@ -18,7 +25,6 @@ class sanity(BaseCase):
 
     def test_text_fields(self):
         # Type text into various text fields and then assert
-        self.open(config.base_url)
         self.type(txt_inputs.text_input_field, "Hello all!")
         self.type(txt_inputs.textarea, "I'm Kfir,\n Kfir Kanizo")
         self.type(txt_inputs.prefilled_text_field, "And I'm typing text.")
@@ -27,7 +33,6 @@ class sanity(BaseCase):
         self.assert_text("And I'm typing text.", txt_inputs.prefilled_text_field)
 
     def test_drop_down(self):
-        self.open(config.base_url)
         # Hover & click a dropdown element and assert results
         self.assert_text("Automation Practice", general.dynamic_dropdown_h3)
         try:
@@ -38,19 +43,16 @@ class sanity(BaseCase):
         self.assert_text("Link Two Selected", general.dynamic_dropdown_h3)
 
     def test_click(self):
-        self.open(config.base_url)
         # Click a button and then verify the expected results
         self.assert_text("This Text is Green", clicks.paragraph_with_text)
         self.click(clicks.button)
         self.assert_text("This Text is Purple", clicks.paragraph_with_text)
 
     def test_svg_visibility(self):
-        self.open(config.base_url)
         # Assert that the given SVG is visible on the page
         self.assert_element(general.html_svg_with_rect)
 
     def test_slider(self):
-        self.open(config.base_url)
         # Verify that a slider control updates a progress bar
         self.assert_element(slider.get_progress("50"))
         self.press_down_arrow(slider.my_slider, times=5)
@@ -59,14 +61,12 @@ class sanity(BaseCase):
         self.assert_element(slider.get_progress("100"))
 
     def test_select_drop_down(self):
-        self.open(config.base_url)
         # Verify that a "select" option updates a meter bar
         self.assert_element(dropdowns.quarter_meter)
         self.select_option_by_text(dropdowns.select_drop_down, "Set to 75%")
         self.assert_element(dropdowns.three_quarters_quarter)
 
     def test_iframe_img(self):
-        self.open(config.base_url)
         # Assert an element located inside an iFrame
         self.assert_false(self.is_element_visible("img"))
         self.switch_to_frame(iframes.img_iframe)
@@ -74,7 +74,6 @@ class sanity(BaseCase):
         self.switch_to_default_content()
 
     def test_iframe_text(self):
-        self.open(config.base_url)
         # Assert text located inside an iFrame
         self.assert_false(self.is_text_visible("iFrame Text"))
         self.switch_to_frame(iframes.txt_iframe)
@@ -82,14 +81,12 @@ class sanity(BaseCase):
         self.switch_to_default_content()
 
     def test_radio_button(self):
-        self.open(config.base_url)
         # Verify that clicking a radio button selects it
         self.assert_false(self.is_selected(radio_buttons.rb_two))
         self.click(radio_buttons.rb_two)
         self.assert_true(self.is_selected(radio_buttons.rb_two))
 
     def test_check_box(self):
-        self.open(config.base_url)
         # Verify that clicking a checkbox makes it selected
         self.assert_element_not_visible(checkboxs.logo)
         self.assert_false(self.is_selected(checkboxs.check_box_one))
@@ -98,7 +95,6 @@ class sanity(BaseCase):
         self.assert_element(checkboxs.logo)
 
     def test_multiple_checkbox(self):
-        self.open(config.base_url)
         # Verify clicking on multiple elements with one call
         self.assert_false(self.is_selected(checkboxs.check_box_two))
         self.assert_false(self.is_selected(checkboxs.check_box_three))
@@ -109,7 +105,6 @@ class sanity(BaseCase):
         self.assert_true(self.is_selected(checkboxs.check_box_four))
 
     def test_iframe_checkbox(self):
-        self.open(config.base_url)
         # Verify that clicking an iFrame checkbox selects it
         self.assert_false(self.is_element_visible(checkboxs.check_box_six))
         self.switch_to_frame(iframes.checkbox_iframe)
@@ -120,7 +115,6 @@ class sanity(BaseCase):
         self.switch_to_default_content()
 
     def test_drag_and_drop(self):
-        self.open(config.base_url)
         # Verify Drag and Drop
         self.click(checkboxs.check_box_one)
         self.assert_element_not_visible(drag_drops.element_in_place)
@@ -128,7 +122,6 @@ class sanity(BaseCase):
         self.assert_element(drag_drops.element_in_place)
 
     def test_link_text(self):
-        self.open(config.base_url)
         # Assert link text
         self.assert_link_text("seleniumbase.com")
         self.assert_link_text("SeleniumBase on GitHub")
@@ -137,11 +130,9 @@ class sanity(BaseCase):
         self.click_link("SeleniumBase Demo Page")
 
     def test_exact_text(self):
-        self.open(config.base_url)
         # Assert exact text
         self.assert_exact_text("Demo Page", general.demo_page_h1)
 
     def test_highlight_element(self):
-        self.open(config.base_url)
         # Highlight a page element (Also asserts visibility)
         self.highlight("h2")
